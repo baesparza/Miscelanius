3b8baa8503b0bdac822ed0a63aa03e309e49b387
baesparza <bruno.be81@gmail.com>
2017-05-14 19:25:59 -0500

    added ng-class module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { NgStyleComponent } from './components/ng-style/ng-style.component';
 import { CssComponent } from './components/css/css.component';
+import { NgClassComponent } from './components/ng-class/ng-class.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NgStyleComponent,
-    CssComponent
+    CssComponent,
+    NgClassComponent
   ],
   imports: [
     BrowserModule,
