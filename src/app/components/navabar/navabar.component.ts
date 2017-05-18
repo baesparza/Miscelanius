6cf0935771e280b3681078e281c311f2477ee587
baesparza <bruno.be81@gmail.com>
2017-05-17 21:40:27 -0500

    Navbar component

---
+++ src/app/components/navabar/navabar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navabar',
+  templateUrl: './navabar.component.html',
+  styles: []
+})
+export class NavabarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
