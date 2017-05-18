f5c75513f7fb3e03c4a1632f25be0ed3cfee2e09
baesparza <bruno.be81@gmail.com>
2017-05-17 21:41:11 -0500

    App Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { APP_ROUTING } from './app.routes';
+
 import { AppComponent } from './app.component';
 import { NgStyleComponent } from './components/ng-style/ng-style.component';
 import { CssComponent } from './components/css/css.component';
@@ -10,6 +12,12 @@ import { NgClassComponent } from './components/ng-class/ng-class.component';
 import { AsincronoComponent } from './components/asincrono/asincrono.component';
 import { ResaltadoDirective } from './directives/resaltado.directive';
 import { SwichComponent } from './components/swich/swich.component';
+import { HomeComponent } from './components/home/home.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
+import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
+import { NavabarComponent } from './components/navabar/navabar.component';
 
 @NgModule({
   declarations: [
@@ -19,12 +27,19 @@ import { SwichComponent } from './components/swich/swich.component';
     NgClassComponent,
     AsincronoComponent,
     ResaltadoDirective,
-    SwichComponent
+    SwichComponent,
+    HomeComponent,
+    UsuarioComponent,
+    UsuarioEditarComponent,
+    UsuarioDetalleComponent,
+    UsuarioNuevoComponent,
+    NavabarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
